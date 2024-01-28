5e0572939a48299df2bef5c03a5a6c7053f71aa1
ErikWe <erikwestberg7@gmail.com>
2024-01-28 18:17:44 +0100

    Migrated from SharpAttributeParser/SharpAttributeParser

---
+++ src/SharpAttributeParser.RecordBuilders.Core/IRecordBuilder.cs
@@ -0,0 +1,10 @@
+﻿namespace SharpAttributeParser;
+
+/// <summary>Responsible for building attribute records.</summary>
+/// <typeparam name="TRecord">The type of the built attribute record.</typeparam>
+public interface IRecordBuilder<out TRecord>
+{
+    /// <summary>Builds an attribute record.</summary>
+    /// <returns>The built attribute record.</returns>
+    public abstract TRecord Build();
+}
---
+++ src/SharpAttributeParser.RecordBuilders.Core/README.nupkg.md
@@ -0,0 +1,5 @@
+# SharpAttributeParser.RecordBuilders.Core
+
+Adds record-builders to `SharpAttributeParser`.
+
+See [GitHub](https://github.com/SharpAttributeParser/SharpAttributeParser.RecordBuilders) for more information.
